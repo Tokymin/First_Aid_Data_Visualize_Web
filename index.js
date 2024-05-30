d3a305e47390ca126e9c85511c77d37b18e1b92a
Tokymin <466350259@qq.com>
2024-05-30 11:08:22 +0800

    成功运行一个echarts的图表示例

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+
+// 设置视图引擎为EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', { title: 'ECharts Example' });
 });
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Example app listening at http://0.0.0.0:${port}`);
-  });
+  console.log(`Example app listening at http://0.0.0.0:${port}`);
+});
